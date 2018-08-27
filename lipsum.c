a372800f8fa894d735b1027f243634ab9be8ee3a
George Crabtree <supercrabtree@googlemail.com>
2018-08-28 00:08:41 +1000

    Allow printing of any number of words

--- lipsum.c
+++ lipsum.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char loremIpsum[] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+char loremIpsum[] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
 void usage() {
 	fprintf(stderr, "usage: lorem [numwords]\n");
@@ -20,6 +20,27 @@ int isInt(char *string) {
 	return 1;
 }
 
+void printXWordsOfLipsum(int numToPrint) {
+	int numPrinted = 0;
+	int count = 0;
+	int c = loremIpsum[count++];
+	while (numPrinted < numToPrint) {
+		if (c == '\0') {
+			count = 0;
+			c = loremIpsum[count++];
+		}
+		if (c == ' ') {
+			++numPrinted;
+			putchar(c);
+		} else if ((c == '.' || c == ',') && numPrinted != numToPrint) {
+			putchar(c);
+		} else {
+			putchar(c);
+		}
+		c = loremIpsum[count++];
+	}
+}
+
 int main(int argc, char **argv) {
 	if (argc == 1) {
 		printf("%s", loremIpsum);
@@ -33,4 +54,8 @@ int main(int argc, char **argv) {
 	if (!isInt(argv[1])) {
 		usage();
 	}
+
+	int numWords = atoi(argv[1]);
+	printXWordsOfLipsum(numWords);
+	exit(EXIT_SUCCESS);
 }
